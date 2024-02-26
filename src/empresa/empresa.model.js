f41d101e2effabdd4a07a8322560a5cf84e4173f
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-02-26 17:24:52 -0600

    Se crea el modelo de empresa

---
+++ src/empresa/empresa.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const EmpresaSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: [true, "The name is required"],
+  },
+  levelImpact: {
+    type: Number,
+    required: [true, "The level of impact is required"],
+    unique: true,
+  },
+  yearsTrayectory: {
+    type: Number,
+    required: [true, "The years of trayectory is required"],
+  },
+  categoria: {
+    type: String,
+    required: [true, "The category is required"],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  }
+});
+
+UserSchema.methods.toJSON = function(){
+  const { __v, _id, ...empresa} = this.toObject();
+  empresa.uid = _id;
+  return empresa;
+}
+
+export default mongoose.model('Empresa', EmpresaSchema);
